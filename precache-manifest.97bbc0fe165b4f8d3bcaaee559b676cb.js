ed066190d8099cf12df6d3fc5390056130b81582
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-11-27 22:40:35 -0500

    Updates

---
+++ precache-manifest.97bbc0fe165b4f8d3bcaaee559b676cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "46fc4f075aad54f12af8",
+    "url": "/todo-app/static/js/main.46fc4f07.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/todo-app/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "46fc4f075aad54f12af8",
+    "url": "/todo-app/static/css/main.5ad55395.chunk.css"
+  },
+  {
+    "revision": "d7166716b38fc54fbde492e02f8fa586",
+    "url": "/todo-app/index.html"
+  }
+];
